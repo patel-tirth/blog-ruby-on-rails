cf3f6a40dfa890956cafe6f1ab0a6660d975a468
patel-tirth <tpatel79@uic.edu>
2022-01-21 20:46:58 -0600

    edit,update,show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,25 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated!"
+            redirect_to article_path(@article)
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
+
     def create
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:notice] = "Article was successfully created!"
             redirect_to article_path(@article)
         else
             render 'new', status: :unprocessable_entity
