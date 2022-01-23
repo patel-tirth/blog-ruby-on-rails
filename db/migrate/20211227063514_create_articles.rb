dbeef267f509d0e5ad7825c70930f31e14d2b484
patel-tirth <tpatel79@uic.edu>
2022-01-23 14:59:17 -0600

    Add association user article

--- db/migrate/20211227063514_create_articles.rb
+++ db/migrate/20211227063514_create_articles.rb
@@ -1,8 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
-    create_table :articles do |t|
-      t.string :title
-      t.timestamps
+    if !table_exists?(:articles)
+      create_table :articles do |t|
+        t.string :title
+        t.timestamps
+    end
     end
   end
 end
