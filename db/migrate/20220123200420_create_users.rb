efe82ef3d35c140d44fbdad26bd392896d0a850b
patel-tirth <tpatel79@uic.edu>
2022-01-23 23:06:48 -0600

    update

--- db/migrate/20220123200420_create_users.rb
+++ db/migrate/20220123200420_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    if !table_exists?(:users)
+
       create_table :users do |t|
         t.string :username
         t.string :email
         t.timestamps
-    end
+
     end
   end
 end
