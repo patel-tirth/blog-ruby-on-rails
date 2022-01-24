00fd92b67eccfbf962fd58e08964c0a4c36c7ccc
patel-tirth <tpatel79@uic.edu>
2022-01-23 23:14:39 -0600

    sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to Blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new', status: :unprocessable_entity
         end
